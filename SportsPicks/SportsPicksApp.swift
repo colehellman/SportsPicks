64e6f9da95617d4cc0599c7228d739a81d980c09
colehellman <cole.hellman@gmail.com>
2024-11-18 21:27:08 -0600

    Initial Commit

---
+++ SportsPicks/SportsPicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsPicksApp.swift
+//  SportsPicks
+//
+//  Created by Cole Hellman on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SportsPicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportsPicksTests/SportsPicksTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportsPicksTests.swift
+//  SportsPicksTests
+//
+//  Created by Cole Hellman on 11/18/24.
+//
+
+import Testing
+@testable import SportsPicks
+
+struct SportsPicksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
